bb4c0611872779df9c54c9d2e0b355dd6dde131b
Power70 <nwisuanu@gmail.com>
2024-10-03 17:51:57 +0100

    modify final user file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,17 @@
 /* eslint-disable */
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  filename
-) {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename),
-  ]).then((res) =>
-    res.map((o) => ({
-      status: o.status,
-      value: o.status === "fulfilled" ? o.value : String(o.reason),
-    }))
-  );
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  const results = await Promise.allSettled([signUpPromise, uploadPromise]);
+
+  return results.map(result => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
+
