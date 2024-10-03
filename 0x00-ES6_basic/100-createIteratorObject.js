c2f1ba344100633d072cfcababf9bba167501684
Power70 <nwisuanu@gmail.com>
2024-10-03 17:14:41 +0100

    created an iterator

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+export default function createIteratorObject(report) {
+    const employees = Object.values(report.allEmployees).flat(); 
+    let index = 0; 
+  
+    return {
+      next: function() {
+        if (index < employees.length) {
+          return { value: employees[index++], done: false }; 
+        }
+        return { done: true }; 
+      },
+      [Symbol.iterator]: function() {
+        return this; 
+      }
+    };
+  }
+  
