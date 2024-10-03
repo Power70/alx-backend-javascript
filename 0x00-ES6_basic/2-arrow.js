c9093a256e2c676632a9ac858bb5664f08213f4a
Power70 <nwisuanu@gmail.com>
2024-10-03 15:00:36 +0100

    reworked on arrow function file

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
     };
   }
