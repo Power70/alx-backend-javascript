bf186dd41ac53b368481fcd0fa1447338627c22c
Power70 <nwisuanu@gmail.com>
2024-10-10 17:14:32 +0100

    added a new file that returns an array of object

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,10 +3,10 @@
  *
  * @return {Array<Object>} An array of student objects.
  */
-export default function getListStudents(){
+export default function getListStudents() {
     return [
-        {id: 1, firstName: 'Guillaume', Location: 'San Francisco'},
-        {id: 2, firstName: 'James', Location: 'Columbia'},
-        {id: 5, firstName: 'Serena', Location: 'San Francisco'},
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
-}
+  }
