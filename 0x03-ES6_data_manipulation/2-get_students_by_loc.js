ebc79ff3062e0dfd5e3c5735bb4f5778c8a9f75f
Power70 <nwisuanu@gmail.com>
2024-10-10 19:42:15 +0100

    updated node modules

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -4,7 +4,6 @@
  * @param {string} city - The city to filter students by.
  * @returns {Array} - An array of students located in the specified city.
  */
-export default function getStudentsByLocation(students, city) {
-    return students.filter(student => student.location === city);
-  }
-  
+const getStudentsByLocation = (students, city) => {
+  return students.filter((student) => student.location === city);
+};
