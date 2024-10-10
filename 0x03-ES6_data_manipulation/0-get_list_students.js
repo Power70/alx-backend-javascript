f30986935e3773cae3668cffe4fc32d6b6d223d4
Power70 <nwisuanu@gmail.com>
2024-10-10 17:06:53 +0100

    added a new file that returns an array of object

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Returns an array of student objects with their respective IDs, first names, and locations.
+ *
+ * @return {Array<Object>} An array of student objects.
+ */
+export default function getListStudents(){
+    return [
+        {id: 1, firstName: 'Guillaume', Location: 'San Francisco'},
+        {id: 2, firstName: 'James', Location: 'Columbia'},
+        {id: 3, firstName: 'Serena', Location: 'San Francisco'},
+    ]
+}
