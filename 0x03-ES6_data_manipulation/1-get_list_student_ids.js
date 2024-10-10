3827452a89b36de0c99b42f9cc2e9bf3b8041344
Power70 <nwisuanu@gmail.com>
2024-10-10 18:21:33 +0100

    a function thar return an array of student id

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,7 @@
 /** This function iterate through an array of student and return only student ID */
-export default function getListStudentIds(students){
-
-    if (!Array.isArray(students)){
-        return [];
-    }
-    return students.map(student => student.id);
-  
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
 }
