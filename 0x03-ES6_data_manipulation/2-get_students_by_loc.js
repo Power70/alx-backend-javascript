1f2e64513cd2db73dc02de69ffbb0af094bf6790
Power70 <nwisuanu@gmail.com>
2024-10-10 19:01:31 +0100

    added a file that return students in same city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+/**
+ * This function filters students based on their location.
+ * @param {Array} students - An array of student objects.
+ * @param {string} city - The city to filter students by.
+ * @returns {Array} - An array of students located in the specified city.
+ */
+export default function getStudentsByLocation(students, city) {
+    return students.filter(student => student.location === city);
+  }
+  
